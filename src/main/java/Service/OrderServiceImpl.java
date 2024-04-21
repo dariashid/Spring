6c239977fa7434a411995011284b859012c02804
lvarezhkal <kkakxaa@gmail.com>
2024-04-21 17:14:20 +0300

    Spring

---
+++ src/main/java/Service/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    private final  Cart cart;
+
+    public OrderServiceImpl(Cart cart) {
+        this.cart = cart;
+    }
+
+    @Override
+    public List<Integer> addItems(List<Integer> items) {
+        return cart.addItems(items);
+    }
+
+    @Override
+    public List<Integer> getItems() {
+        return cart.getItems();
+    }
+}
